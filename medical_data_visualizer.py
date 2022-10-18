2f9a060c0f7bccf7abfc99043521e8fb9c979346
Thiago Waslawick Batista <100001413+BatistaT@users.noreply.github.com>
2022-10-18 08:47:06 -0300

    update project

---
+++ medical_data_visualizer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = None
+
+df['overweight'] = None
+
+def draw_cat_plot():
+    df_cat = None
+    df_cat = None
+
+    fig.savefig('catplot.png')
+    return fig
+
+
+def draw_heat_map():
+    df_heat = None
+    corr = None
+    mask = None
+
+    fig, ax = None
+
+    fig.savefig('heatmap.png')
+    return fig
